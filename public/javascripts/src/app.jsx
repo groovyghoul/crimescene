25c0a93f33267d9c13d15e193be1a582eed2811d
Richard O'Neil <richard@bytemares.com>
2015-10-10 22:16:18 -0300

    Update bower component folder
    * installed jquery and bootstrap
    * added an absolutely butchered index (layout is messed up)

--- public/javascripts/src/app.jsx
+++ public/javascripts/src/app.jsx
@@ -1,4 +1,6 @@
-var React = require('react');
+var $ = jQuery = require('../../components/jquery/dist/jquery');
+//var bootstrap = require('../../components/bootstrap/assets/javascripts/bootstrap');
+var React = require('../../components/react/react.js');
 var HelloWorld = require('./HelloWorld.jsx');
 
 React.render(
